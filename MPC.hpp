9235323e26bf8e19e09b657ca129887ba171ae6f
Gyrydymptum <gyrydymptum@gmail.com>
2021-12-05 17:49:01 +0100

    c-tor,members

--- MPC.hpp
+++ MPC.hpp
@@ -1,5 +1,20 @@
 #pragma once
-
+#include <string>
 class MPC {
+public:
+    MPC(const std::string& srv_address, const std::string& port, const std::string& db_name, const std::string& user, const std::string& password)
+        : srv_address_(srv_address)
+        , port_(port)
+        , db_name_(db_name)
+        , user_(user)
+        , password_(password)
+    { std::cout << "MPC C-tor\n";}
 
+private:
+    const std::string srv_address_;
+    const std::string port_;
+    const std::string db_name_;
+    const std::string user_;
+    const std::string password_;
+    bool connection_;
 };
